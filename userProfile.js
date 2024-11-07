4cc9aa7bbeebd7b1277adc5d043dbfd56c1ef2df
Clarence21-ai <agtualclarence16@gmail.com>
2024-11-07 16:09:17 +0000

    Initial commit with app.js

---
+++ userProfile.js
@@ -0,0 +1,8 @@
+let userProfile = {
+    fullName: "John Doe",
+        age: 25,
+            favoriteNumber: 7,
+                favoriteColors: ["red", "blue", "green"]
+                };
+     
+                console.log(userProfile);
